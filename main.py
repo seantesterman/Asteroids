268c005fcfcbc4c03ce793e5a52acf11de987aea
seantesterman <seantesterman@gmail.com>
2024-10-01 18:36:25 -0600

    2.0 Frame Rate

--- main.py
+++ main.py
@@ -4,17 +4,21 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        pygame.Surface.fill(screen, color="black")
+        # Screen
+        screen.fill("black")
         pygame.display.flip()
+
+        # Frame Rate
+        dt = clock.tick(60) / 1000
+        
 
 if __name__ == "__main__":
     main()
